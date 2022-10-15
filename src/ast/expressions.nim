a5d44ccf29af38e8a1cec3b6edf7916c208fac4a
Technisha <proxneon960@gmail.com>
2022-10-15 16:35:55 +0100

    Fixed expressions.nim

--- src/ast/expressions.nim
+++ src/ast/expressions.nim
@@ -1,10 +1,6 @@
 import ../tokens
 
 type Expr* = object of RootObj # So it's inheritable
-  Binary* = object of Expr
-  left*: Expr
-  operator*: Token
-  right*: Expr
 
 proc new(_: typedesc[Binary], left: Expr, operator: Token, right: Expr): Binary =
   return Binary(left: left, operator: operator, right: right)
--- tools/generateast.nim
+++ tools/generateast.nim
@@ -18,10 +18,6 @@ proc defineAst(outputDir, baseObject: string, astTypesArr: openArray[string]) =
   var code = """import ../tokens
 
 type Expr* = object of RootObj # So it's inheritable
-  Binary* = object of Expr
-  left*: Expr
-  operator*: Token
-  right*: Expr
 
 proc new(_: typedesc[Binary], left: Expr, operator: Token, right: Expr): Binary =
   return Binary(left: left, operator: operator, right: right)
