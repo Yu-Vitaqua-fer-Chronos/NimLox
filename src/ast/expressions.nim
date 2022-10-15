7426779e82f60c8a59db75aa258daf3371c315f7
Technisha <proxneon960@gmail.com>
2022-10-15 16:34:33 +0100

    Current work

---
+++ src/ast/expressions.nim
@@ -0,0 +1,38 @@
+import ../tokens
+
+type Expr* = object of RootObj # So it's inheritable
+  Binary* = object of Expr
+  left*: Expr
+  operator*: Token
+  right*: Expr
+
+proc new(_: typedesc[Binary], left: Expr, operator: Token, right: Expr): Binary =
+  return Binary(left: left, operator: operator, right: right)
+
+type Unary* = object of Expr
+  operator*: Token
+  right*: Expr
+
+proc new*(_: typedesc[Unary], operator: Token, right: Expr): Unary =
+  return Unary(operator: operator, right: right)
+
+type Literal* = object of Expr
+  value*: string
+
+proc new*(_: typedesc[Literal], value: string): Literal =
+  return Literal(value: value)
+
+type Binary* = object of Expr
+  left*: Expr
+  operator*: Token
+  right*: Expr
+
+proc new*(_: typedesc[Binary], left: Expr, operator: Token, right: Expr): Binary =
+  return Binary(left: left, operator: operator, right: right)
+
+type Grouping* = object of Expr
+  expression*: Expr
+
+proc new*(_: typedesc[Grouping], expression: Expr): Grouping =
+  return Grouping(expression: expression)
+
